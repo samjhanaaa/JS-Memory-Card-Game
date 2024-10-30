9123b96ba5fb1f7cb51ace1820b8a67bdb25cd43
Samjhana Neupane <samjhanaa.neupane@gmail.com>
2024-10-31 00:58:31 +0545

    added images to each div

--- src/script.js
+++ src/script.js
@@ -1,20 +1,31 @@
 const card = document.getElementsByClassName("cards");
 const cardPicture = [
-    '../ assets / dino.jpg',
-    '../ assets / dolphin.jpg',
-    '../ assets / duck.jpg',
-    '../ assets / elephant.jpg',
-    '../ assets / monkey.jpg',
-    '../ assets / penguin.jpg',
-    '../ assets / sheep.jpg',
-    '../ assets / turtle.jpg'
+    '../assets/dino.jpg',
+    '../assets/dolphin.jpg',
+    '../assets/duck.jpg',
+    '../assets/elephant.jpg',
+    '../assets/monkey.jpg',
+    '../assets/penguin.jpg',
+    '../assets/sheep.jpg',
+    '../assets/turtle.jpg'
 ]
 
+const doubleImages = [...cardPicture, ...cardPicture]
+
 for (i = 0; i < card.length; i++) {
-    card[i].addEventListener("click", function () {
-        this.style.backgroundColor = "red"
-    })
-}
+    const image = document.createElement("img");
+    const initialImage = doubleImages[i];
+
+    image.src = initialImage;
+    image.style.width = "100px";
+    image.style.height = "100px";
+    card[i].appendChild(image);
+
+};
+
+
+
+
 
 
 
